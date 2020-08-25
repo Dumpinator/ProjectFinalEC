b4a8719d42314c00eb4e110117463b5cf43e2085
Digidelphine <gaspardelphine01@gmail.com>
2020-08-25 15:53:02 +0200

    test add class component

---
+++ front-react/src/components/Welcome/Welcome.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from '../../logo.svg'
+import '../../App.css'
+
+class Welcome extends React.Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Welcome in react !
+                </p>
+                <a className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        )
+    }
+}
+
+export default Welcome
